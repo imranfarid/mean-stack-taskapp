618ab236fe84b884985edd0db2ccc704c480bcc9
imranfarid <imran.cyborg@gmail.com>
2017-05-29 18:27:02 +0600

    mongodb mlab connection failed

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://imranfarid:mypassword123!@ds061777.mlab.com:61777/taskapp_imran', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-    res.send('TASK API');
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
